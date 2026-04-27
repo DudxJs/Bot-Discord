b352e369d22abf1a2e4e0d91afbe9cd5a5aba7bd
DudxJs <eduardosantschocolate@gmail.com>
2026-04-27 12:38:07 -0300

    atualizando slash commands para guild

--- deploy-commands.js
+++ deploy-commands.js
@@ -42,7 +42,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
     console.log('🔄 Registrando comandos...');
 
     await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
       { body: commands }
     );
 
