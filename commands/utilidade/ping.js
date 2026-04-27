c8c59b8cd1785642b668557ad97949d06855f819
Dudx_js <eduardosantschocolate@gmail.com>
2026-04-27 02:28:55 -0300

    Delete commands directory

--- commands/utilidade/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'Verifica a latência do bot',
-  async execute(message, args, client) {
-    const ping = Date.now() - message.createdTimestamp;
-    message.reply(`🏓 Pong! Latência: ${ping}ms`);
-  }
-};
